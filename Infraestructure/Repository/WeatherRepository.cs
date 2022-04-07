e9f5a093fe1ed62b2ea049a05f3038c3da506814
Dierish05 <85861227+Dierish05@users.noreply.github.com>
2022-04-07 01:08:40 -0600

    tercer commit

---
+++ Infraestructure/Repository/WeatherRepository.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Domain.interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Infraestructure.Repository
+{
+    public class WeatherRepository : IConsulta
+    {
+        public Root getWeather(string city, string APIKey)
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", city, APIKey);
+                var json = web.DownloadString(url);
+                Root info = JsonConvert.DeserializeObject<Root>(json);
+
+                return info;
+            }
+        }
+
+        public string GetImageLocation(Root info)
+        {
+            string ImageLocation = "https://openweathermap.org/img/w/" + info.weather[0].Icon + ".png";
+            return ImageLocation;
+        }
+
+        public DateTime convertDateTime(long sec)
+        {
+            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).ToLocalTime();
+            day = day.AddSeconds(sec).ToLocalTime();
+
+            return day;
+        }
+
+        public string GetTempinC(Root info)
+        {
+            string temp = (info.main.Temp - 273.15).ToString();
+            return temp;
+        }
+    }
+}

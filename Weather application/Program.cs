f319242fe480c514383365f9624e882ce0771dc5
Dierish05 <85861227+Dierish05@users.noreply.github.com>
2022-04-07 01:10:03 -0600

    quinto commit

---
+++ Weather application/Program.cs
@@ -0,0 +1,34 @@
+﻿using AppCore.IServices;
+using AppCore.Services;
+using Autofac;
+using Domain.interfaces;
+using Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Weather_application.Forms;
+
+namespace Weather_application
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            var builder = new ContainerBuilder();
+            builder.RegisterType<WeatherRepository>().As<IConsulta>();
+            builder.RegisterType<ConsultaServices>().As<IConsultaServices>();
+            var container = builder.Build();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmWeather(container.Resolve<IConsultaServices>()));
+        }
+    }
+}
